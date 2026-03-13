860300477e50bcc6b668cb9253e80f6f0b8a7c32
melaniee14 <hammond.me@northeastern.edu>
2026-03-13 19:47:23 -0400

    hello

--- src/app/labs/lab4/redux/page.tsx
+++ src/app/labs/lab4/redux/page.tsx
@@ -1,18 +1,23 @@
+"use client";
 import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./hello";
+import store from "../store";
+import { Provider } from "react-redux";
 
 export default function ReduxExamples() {
     return (
-      <div>
-        <h2>Redux Examples</h2>
+      <Provider store={store}>
+        <div>
+          <h2>Redux Examples</h2>
 
-        <HelloRedux/>
+          <HelloRedux/>
 
-        <CounterRedux/>
+          <CounterRedux/>
 
-        <AddRedux/>
-      </div>
+          <AddRedux/>
+        </div>
+      </Provider>
     );
    }
    
