91766fd23c5ec48df81df3c614cfb3dc64baafdc
melaniee14 <hammond.me@northeastern.edu>
2026-03-22 23:04:06 -0400

    hello

--- src/app/(kambaz)/courses/[cid]/assignments/reducer.ts
+++ src/app/(kambaz)/courses/[cid]/assignments/reducer.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import { redirect } from "next/navigation";
 
 const initialState = {
-  assignments: assignments,
+  assignments: assignments as any [],
 };
 
 const assignmentsSlice = createSlice({
