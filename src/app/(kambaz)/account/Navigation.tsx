f9003ea5da0d484ea25a46aa96ba57efc634c162
melaniee14 <hammond.me@northeastern.edu>
2026-03-31 23:40:23 -0400

    a6 stuff

--- src/app/(kambaz)/account/Navigation.tsx
+++ src/app/(kambaz)/account/Navigation.tsx
@@ -7,14 +7,24 @@ import { RootState } from "../store";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
   const links = currentUser ? ["profile"] : ["signin", "signup"];
- const pathname = usePathname();
- return (
-   <Nav variant="pills">
-     {links.map((link) => (
-       <NavItem key={link}>
-         <NavLink as={Link} href={link} active={pathname.endsWith(link)}>
-           {link} </NavLink> </NavItem>
-     ))}
-   </Nav>
-);}
+  const pathname = usePathname();
+  return (
+    <Nav variant="pills">
+      {links.map((link) => (
+        <NavItem key={link}>
+          <NavLink as={Link} href={link} active={pathname.endsWith(link)}>
+            {link} </NavLink>
+          
+
+        </NavItem>
+
+      ))}
+
+      <NavItem>
+      {currentUser && currentUser.role === "ADMIN" && (
+            <NavLink as={Link} href={`/account/users`} active={pathname.endsWith('users')}> Users </NavLink>)}
+      </NavItem>
+    </Nav>
+  );
+}
 
--- src/app/(kambaz)/courses/[cid]/people/table/page.tsx
+++ src/app/(kambaz)/courses/[cid]/people/table/page.tsx
@@ -1,36 +1,46 @@
 "use client";
 
-
-import * as db from "../../../../database";
-import { useParams } from "next/navigation";
-
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "./Details";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function PeopleTable() {
-    const { cid } = useParams();
-    const users = db.users;
-    const enrollments = db.enrollments;
+export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
+
   
  return (
   <div id="wd-people-table">
-    
+         {showDetails && (
+       <PeopleDetails
+         uid={showUserId}
+         onClose={() => {
+           setShowDetails(false);
+           fetchUsers();
+         }}/>
+     )}
+
    <Table striped>
     <thead>
      <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
     </thead>
     
     <tbody>
-  {users
-    .filter((usr: { _id: any; }) =>
-      enrollments.some((enrollment: { user: any; course: any; }) => enrollment.user === usr._id && enrollment.course === cid)
-    )
-    .map((user: any) => (
+  {users.map((user: any) => (
       <tr key={user._id}>
         <td className="wd-full-name text-nowrap">
+        <span className="text-decoration-none"
+                 onClick={() => {
+                   setShowDetails(true);
+                   setShowUserId(user._id);
+                 }} >
+
           <FaUserCircle className="me-2 fs-1 text-secondary" />
           <span className="wd-first-name">{user.firstName}</span>
           <span className="wd-last-name">{user.lastName}</span>
+          </span>
         </td>
         <td className="wd-login-id">{user.loginId}</td>
         <td className="wd-section">{user.section}</td>
