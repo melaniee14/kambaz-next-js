58d101d70a09fadb95a0d552bbc210cc01d79fd5
LondonJones <londonjonez00@gmail.com>
2026-04-14 22:12:23 -0400

    quiz structure and auth drop down

--- src/app/(kambaz)/account/signup/page.tsx
+++ src/app/(kambaz)/account/signup/page.tsx
@@ -4,25 +4,58 @@ import { redirect } from "next/navigation";
 import { setCurrentUser } from "../reducer";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
-import { FormControl, Button } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
 import * as client from "../client";
 
 export default function Signup() {
-  const [user, setUser] = useState<any>({username: "", password: ""});
+  const [user, setUser] = useState<any>({ username: "", password: "", role: "STUDENT" });
   const dispatch = useDispatch();
+
   const signup = async () => {
     const currentUser = await client.signup(user);
     dispatch(setCurrentUser(currentUser));
     redirect("/account/profile");
   };
+
   return (
     <div className="wd-signup-screen">
       <h1>Sign up</h1>
-      <FormControl value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })}
-             className="wd-username b-2" placeholder="username" />
-      <FormControl value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })}
-             className="wd-password mb-2" placeholder="password" type="password"/>
-      <button onClick={signup} className="wd-signup-btn btn btn-primary mb-2 w-100"> Sign up </button><br />
-      <Link href="/account/signin" className="wd-signin-link">Sign in</Link>
+
+      <FormControl
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        className="mb-2"
+        placeholder="username"
+      />
+
+      <FormControl
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        className="mb-2"
+        placeholder="password"
+        type="password"
+      />
+
+      <select
+        className="form-control mb-2"
+        value={user.role}
+        onChange={(e) => setUser({ ...user, role: e.target.value })}
+      >
+        <option value="STUDENT">Student</option>
+        <option value="FACULTY">Faculty</option>
+      </select>
+
+      <button
+        onClick={signup}
+        className="wd-signup-btn btn btn-primary mb-2 w-100"
+      >
+        Sign up
+      </button>
+
+      <br />
+      <Link href="/account/signin" className="wd-signin-link">
+        Sign in
+      </Link>
     </div>
-);}
+  );
+}
