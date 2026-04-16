f97f4b9bf24328b88d49d82ed45ddd75e7d3fc5f
LondonJones <londonjonez00@gmail.com>
2026-04-16 00:35:36 -0400

    Added local data, added quiz details editor tab, started questions editor tab

--- src/app/(kambaz)/courses/[cid]/quizzes/reducer.ts
+++ src/app/(kambaz)/courses/[cid]/quizzes/reducer.ts
@@ -1,67 +1,77 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { quizzes} from "../../../database";
-import { v4 as uuidv4 } from "uuid";
-import { redirect } from "next/navigation";
+import { quizzes } from "../../../database";
 
 const initialState = {
-  quizzes: quizzes as any [],
+  quizzes: quizzes as any[],
 };
 
 const quizzesSlice = createSlice({
-    name: "quizzes",
-    initialState,
-    reducers: {
-      addQuiz: (state, { payload: quiz }) => {
-        const newQuiz: any = {
-          _id: quiz._id,
-          title: quiz.title,
-          course: quiz.course,
-          available: quiz.available ?? "",
-          due: quiz.due ?? "",
-          points: quiz.points ?? 100,
-          questions: quiz.questions ?? 0,
-          desc: quiz.desc ?? "New Quiz Description",
-          published: quiz.published ?? false,
-          score: quiz.score ?? 0,
-          newQuiz: true,
-          quizType: quiz.quizType ?? "Graded Quiz",
-          assignmentGroup: quiz.assignmentGroup ?? "Quizzes",
-          shuffleAnswers: quiz.shuffleAnswers ?? true,
-          timeLimit: quiz.timeLimit ?? 20,
-          multipleAttempts: quiz.multipleAttempts ?? false,
-          numberOfAttempts: quiz.numberOfAttempts ?? 1,
-          showCorrectAnswers: quiz.showCorrectAnswers ?? "Immediately",
-          accessCode: quiz.accessCode ?? "",
-          oneQuestionAtATime: quiz.oneQuestionAtATime ?? true,
-          webcamRequired: quiz.webcamRequired ?? false,
-          lockQuestionsAfter: quiz.lockQuestionsAfter ?? false
-
-         
-        };
-        state.quizzes = [...state.quizzes, newQuiz] as any;
-
-      },
-
-      deleteQuiz: (state, { payload: quizId }) => {
-        state.quizzes = state.quizzes.filter(
-          (q: any) => q._id !== quizId);
-      },
-      updateQuiz: (state, { payload: quiz }) => {
-        state.quizzes = state.quizzes.map((q: any) =>
-          q._id === quiz._id ? quiz : q         
-        ) as any;
-      },
-      editQuiz: (state, { payload: quizId }) => {
-        state.quizzes = state.quizzes.map((q: any) =>
-          q._id === quizId ? { ...q, editing: true } : q
-        ) as any;
-      },
-
-      setQuizzes: (state, action) => {
-        state.quizzes = action.payload;
-      },
+  name: "quizzes",
+  initialState,
+  reducers: {
+    addQuiz: (state, { payload: quiz }) => {
+      const newQuiz: any = {
+        _id: quiz._id,
+        title: quiz.title,
+        course: quiz.course,
+        available: quiz.available ?? "",
+        due: quiz.due ?? "",
+        points: quiz.points ?? 100,
+        questions: quiz.questions ?? 0,
+        desc: quiz.desc ?? "New Quiz Description",
+        published: quiz.published ?? false,
+        score: quiz.score ?? 0,
+        newQuiz: true,
+        quizType: quiz.quizType ?? "Graded Quiz",
+        assignmentGroup: quiz.assignmentGroup ?? "Quizzes",
+        shuffleAnswers: quiz.shuffleAnswers ?? true,
+        timeLimit: quiz.timeLimit ?? 20,
+        multipleAttempts: quiz.multipleAttempts ?? false,
+        numberOfAttempts: quiz.numberOfAttempts ?? 1,
+        showCorrectAnswers: quiz.showCorrectAnswers ?? "Immediately",
+        accessCode: quiz.accessCode ?? "",
+        oneQuestionAtATime: quiz.oneQuestionAtATime ?? true,
+        webcamRequired: quiz.webcamRequired ?? false,
+        lockQuestionsAfter: quiz.lockQuestionsAfter ?? false,
+      };
+      state.quizzes = [...state.quizzes, newQuiz] as any;
     },
-  });
-  export const { addQuiz, deleteQuiz, updateQuiz, editQuiz, setQuizzes } =
-    quizzesSlice.actions;
-  export default quizzesSlice.reducer;
+
+    deleteQuiz: (state, { payload: quizId }) => {
+      state.quizzes = state.quizzes.filter((q: any) => q._id !== quizId);
+    },
+
+    updateQuiz: (state, { payload: quiz }) => {
+      state.quizzes = state.quizzes.map((q: any) =>
+        q._id === quiz._id ? quiz : q
+      ) as any;
+    },
+
+    editQuiz: (state, { payload: quizId }) => {
+      state.quizzes = state.quizzes.map((q: any) =>
+        q._id === quizId ? { ...q, editing: true } : q
+      ) as any;
+    },
+
+    togglePublish: (state, { payload: quizId }) => {
+      state.quizzes = state.quizzes.map((q: any) =>
+        q._id === quizId ? { ...q, published: !q.published } : q
+      ) as any;
+    },
+
+    setQuizzes: (state, action) => {
+      state.quizzes = action.payload;
+    },
+  },
+});
+
+export const {
+  addQuiz,
+  deleteQuiz,
+  updateQuiz,
+  editQuiz,
+  togglePublish,
+  setQuizzes,
+} = quizzesSlice.actions;
+
+export default quizzesSlice.reducer;
